9e951b5a71d887d968db38f2fff92bbf79da7024
Yun (Ann) Zhou <bookcooklook@gmail.com>
2022-10-19 23:02:33 -0400

    delete bookstore repo due to security alerts

--- Projects 2021/Bookstore Website/bookstore project/bookstore/src/main/java/com/itheima/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.domain.User;
-import org.apache.ibatis.annotations.Param;
-
-public interface UserDao {
-    User findByUsernameAndPwd(@Param("username") String username, @Param("password") String password);
-    User findByUsername(@Param("username") String username);
-}
--- Projects 2021/Bookstore Website/bookstore project/bookstore/src/main/java/com/itheima/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.domain.User;
-
-public interface UserService {
-    User verifyUserLogin(String username, String password);
-
-    boolean findUserByUsername(String username);
-}
